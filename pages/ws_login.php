1c56d6a59e374f546e6c7211e7fb25cfadd534c8
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2021-02-15 22:35:09 +0100

    Update ws_login.php

--- pages/ws_login.php
+++ pages/ws_login.php
@@ -202,7 +202,7 @@ function create_char($player) {
 		'worldid' => 0,
 		'name' => $player['name'],
 		'ismale' => intval($player['sex']) === 1,
-		'tutorial' => (bool)$player['istutorial'], //intval($player['lastlogin']) === 0,
+		'tutorial' => (bool)$player['istutorial'],
 		'level' => intval($player['level']),
 		'vocation' => Website::getVocationName($player['vocation']),
 		'outfitid' => intval($player['looktype']),
